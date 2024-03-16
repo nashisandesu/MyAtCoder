03bb3dba7ccc68623a6a653ff8cdfac207d33515
nashisandesu <n.sweet.lily0294@gmail.com>
2024-03-17 01:07:05 +0900

    abc345参加

---
+++ ABC/abc345/abc345_a.py
@@ -0,0 +1,27 @@
+import sys, bisect
+import math
+# import numpy as np
+from atcoder.lazysegtree import LazySegTree
+from atcoder.segtree import SegTree
+from atcoder.dsu import DSU
+sys.setrecursionlimit(10**8)
+from collections import deque
+from itertools import combinations
+from itertools import permutations
+import heapq
+
+I = lambda: int(input())
+S = lambda: str(input())
+Sl = lambda: list(input())
+Mi = lambda: map(int, input().split())
+Ms = lambda: map(str, input().split())
+Li = lambda: list(map(int, input().split()))
+Ls = lambda: list(map(str, input().split()))
+
+########################################################
+s = Sl()
+
+if s[0] == '<' and s[-1] == '>' and all(c == '=' for c in s[1:-1]) and len(s[1:-1]) >= 1:
+    print('Yes')
+else:
+    print('No')
