290583c71fe1c8751043ecf43c5a7e6f6801525e
nashisandesu <n.sweet.lily0294@gmail.com>
2024-04-01 18:45:51 +0900

    約数列挙関数の追加

--- tools/templete.py
+++ tools/templete.py
@@ -117,3 +117,15 @@ factorization(24)
 
 ## [[2, 3], [3, 1]] 
 ##  24 = 2^3 * 3^1
+
+#約数の列挙
+def make_divisors(n):
+    lower_divisors , upper_divisors = [], []
+    i = 1
+    while i*i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n//i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
