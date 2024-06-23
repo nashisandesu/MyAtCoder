d77de62f07a2ec165c5534bab3b7a9427cb75773
nashisandesu <n.sweet.lily0294@gmail.com>
2024-06-24 02:12:56 +0900

    defaultdictの追加

--- tools/create_contest_file.py
+++ tools/create_contest_file.py
@@ -23,6 +23,7 @@ from atcoder.segtree import SegTree
 from atcoder.dsu import DSU
 sys.setrecursionlimit(10**8)
 from collections import deque
+from collections import defaultdict
 from itertools import combinations
 from itertools import permutations
 from itertools import combinations_with_replacement
