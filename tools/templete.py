d18b29025096d1dfe815416d25bb94c26043a4d8
nashisandesu <n.sweet.lily0294@gmail.com>
2024-08-11 00:05:16 +0900

    add megru_bisect

--- tools/templete.py
+++ tools/templete.py
@@ -129,3 +129,25 @@ def make_divisors(n):
                 upper_divisors.append(n//i)
         i += 1
     return lower_divisors + upper_divisors[::-1]
+
+# めぐる式二分探索
+def is_ok(arg):
+    # 条件を満たすかどうか？問題ごとに定義
+    pass
+
+# [ok, ng) または (ng, ok] 
+def meguru_bisect(ng, ok):
+    '''
+    初期値のng,okを受け取り,is_okを満たす最小(最大)のokを返す
+    まずis_okを定義すべし
+    ng ok は  とり得る最小の値-1 とり得る最大の値+1
+    最大最小が逆の場合はよしなにひっくり返す
+    okは必ず条件を満たし、ngは必ず条件を満たさない
+    '''
+    while (abs(ok - ng) > 1):
+        mid = (ok + ng) // 2
+        if is_ok(mid):
+            ok = mid
+        else:
+            ng = mid
+    return ok
